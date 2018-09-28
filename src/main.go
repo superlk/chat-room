d9125e7c7e454a7b8cdb16daffc3f21187720f63
superlk <651002081@qq.com>
2018-09-28 09:26:09 +0800

    并发聊天室server端

--- src/main.go
+++ src/main.go
@@ -4,23 +4,24 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"time"
 )
 
-type Client struct {
+type Clinet struct {
 	C    chan string // 用户发送数据的管道
 	Name string      // 用户名
 	Addr string      //网络地址
 }
 
 // 保存在线用户，cliAddr ==== > Client
-var onlineMap map[string]Client
+var onlineMap map[string]Clinet
 
 var message = make(chan string)
 
 //新开一个携程，转发消息，只要有消息来了，遍历map，给mao每个成员发送这个消息
 func Manager() {
 	// 给map 新建，分配空间
-	onlineMap = make(map[string]Client)
+	onlineMap = make(map[string]Clinet)
 	for {
 		msg := <-message //没有消息前，这里会堵塞
 		//遍历map，给mao每个成员发送这个消息
@@ -31,14 +32,14 @@ func Manager() {
 
 }
 
-func writeMagToClient(cli Client, conn net.Conn) {
+func writeMagToClient(cli Clinet, conn net.Conn) {
 	for msg := range cli.C { //给当前客户发送信息
 		conn.Write([]byte(msg + "\n"))
 	}
 
 }
 
-func MakeMsg(cli Client, msg string) (buf string) {
+func MakeMsg(cli Clinet, msg string) (buf string) {
 	buf = "[" + cli.Addr + "]" + cli.Name + ":" + msg
 	return
 }
@@ -48,7 +49,7 @@ func HandleConn(conn net.Conn) { // 处理用户链接
 	cliAddr := conn.RemoteAddr().String()
 
 	// 创建一个结构体,默认，用户名和网络地址一样
-	cli := Client{make(chan string), cliAddr, cliAddr}
+	cli := Clinet{make(chan string), cliAddr, cliAddr}
 	// 把结构体添加到map
 	onlineMap[cliAddr] = cli
 
@@ -60,12 +61,16 @@ func HandleConn(conn net.Conn) { // 处理用户链接
 	//提示我是谁
 	cli.C <- MakeMsg(cli, "i am here")
 
+	isQuit := make(chan bool)  // 对方是否主动退出
+	hasData := make(chan bool) //对方是否有数据发送
+
 	// 新建一个携程，接收用户发送过来的的数据
 	go func() {
 		buf := make([]byte, 2048)
 		for {
 			n, err := conn.Read(buf)
-			if n == 0 { // 两种情况，对方端口，或出现问题
+			if n == 0 { // 两种情况，对方断开，或出现问题
+				isQuit <- true //主动退出
 				fmt.Println("conn read error =", err)
 				return
 			}
@@ -90,11 +95,25 @@ func HandleConn(conn net.Conn) { // 处理用户链接
 				message <- MakeMsg(cli, msg)
 
 			}
+
+			hasData <- true //代表有数据
 			// 转发消息
 			//message <- MakeMsg(cli, msg)
 		}
 	}()
 	for {
+		//通过select检测channel的流动
+		select {
+		case <-isQuit:
+			delete(onlineMap, cliAddr)         //从map里移除当前用户
+			message <- MakeMsg(cli, "logout ") //广播谁下线
+			return
+		case <-hasData:
+		case <-time.After(60 * time.Second): // 60s后移除
+			delete(onlineMap, cliAddr)
+			message <- MakeMsg(cli, "time out leave out")
+			return
+		}
 	}
 }
 
